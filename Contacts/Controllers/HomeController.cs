ac5ad611a25e1e5a4d3ba248ed718e1635983f31
Jeremy Jacala <rdnhjnm@gmail.com>
2016-03-22 23:47:37 +0800

    1st Commit
    Notice me senpai!

---
+++ Contacts/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Contacts.Controllers{
+    public class HomeController : Controller{
+        public ActionResult Index(){
+            return View();
+        }
+
+        public ActionResult ListDB(){
+            ViewBag.Message = "I'm still trying to learn to return data from MySQL Database Server";
+
+            return View();
+        }
+
+        public ActionResult List(){
+            ViewBag.Message = "It's Something";
+
+            return View();
+        }
+    }
+}
